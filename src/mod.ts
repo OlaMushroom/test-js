dd25f81b67f736f9bfbd50c6c44f72996da18c47
Nyanto <ola.mushroom@gmail.com>
2024-07-06 21:14:40 +0700

    fix

--- src/mod.ts
+++ src/mod.ts
@@ -9,13 +9,12 @@ import * as local from '@om/nekos-moe';
 //import { cdn } from './import.js';
 
 /**
+ *
  */
 export function pkgLocal(): void {
 	console.log('Local:\n%o', local.info);
 }
 
-/**
- */
 /*
 export function pkgCdn(): void {
 	console.log('CDN:\n%o', cdn.info);
