82e5cdd76a7f311e1076bb59ce92d7d902b88312
Nyanto <ola.mushroom@gmail.com>
2024-08-15 10:39:30 +0700

    up

--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
-import { pkgLocal, pkgCdn } from './src/mod.ts';
-pkgLocal(); pkgCdn();
+import { readFileSync, writeFileSync } from 'node:fs';
+import { parse, decode, objectify } from 'parse-osr';
+//import { pkgLocal, pkgCdn } from './src/mod.ts';
+//pkgLocal(); pkgCdn();
+const replay = readFileSync('./local/r.osr');
+const parsed = parse(replay, true);
+console.log(parsed);
